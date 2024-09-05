8cf024f91c07497a6e10fd6eac5ee2ecf85ccb1c
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-09-05 22:43:19 +0530

    completed with docker

---
+++ ElasticSearch/Services/IElasticService.cs
@@ -0,0 +1,20 @@
+﻿using ElasticSearch.Models;
+
+namespace ElasticSearch.Services
+{
+    public interface IElasticService
+    {
+        Task CreateIndexIfNotExistsAsync(string indexName);
+        Task<bool> AddOrUpdate(User user);
+
+        Task<bool> AddOrUpdateBulk(IEnumerable<User> users, string indexName);
+
+        Task<User> Get(string key);
+
+        Task<List<User>?> GetAll();
+
+        Task<bool> Remove(string key);
+
+        Task<long?> RemoveAll();
+    }
+}
